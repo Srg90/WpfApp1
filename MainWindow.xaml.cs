9f0067b610994db9fc3b3580db617f1ed1db6547
Srg90 <usikov90@mail.ru>
2021-12-14 20:50:24 +0300

    Добавьте файлы проекта.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp19
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public double sum = 0;
+        public Button[] buttons = new Button[18];
+        public string[] NameButtons = new string[18] { "1", "2", "3", "4", "5", "6", "7", "8", "9", "0", "-", "+", "*", "/", "=", ".", "AC", "Sqr" };
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+       
+    }
+}
